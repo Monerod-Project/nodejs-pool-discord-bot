55dfd32329a21971c0a9cdcef37b403740b5d28c
Shay <sgaming24@outlook.com>
2021-12-31 20:42:05 +0000

    Effort in embed & color codes

--- bot.js
+++ bot.js
@@ -20,16 +20,29 @@ function checkForNewBlock() {
 	api.checkForNewBlock(function(newBlock, blockDetails) {
 		if (newBlock) {
 			const channel = client.channels.cache.get(config.CHANNEL_ID);
-		
+			let effort = (blockDetails.shares / blockDetails.diff);
+			
+			/*
+				green    0.5 >= effort > 0
+				yellow   1   >= effort > 0.5
+				red	            effort > 1
+			*/
+
+			let effortRGB = [0, 255, 0]
+			if (effort > 0.5) { effortRGB[0] = 255; }
+			if (effort > 1) { effortRGB[1] = 0; }
+			
 			embed = new Discord.MessageEmbed()
 				.setTitle('New Block Found!')
 				.setURL(config.BLOCK_EXPLORER+blockDetails.height)
-				.setColor(0xff6600) //make it monero orange
-	//				.setDescription('Informative text to add at start')
-				.addField('Hash', blockDetails.hash)
-				.addField('Diff', blockDetails.diff)
-				.addField('Height', blockDetails.height)
-				.addField('Value', blockDetails.value);
+				.setColor(effortRGB) //make it green/yellow/red based on effort
+				// .setColor(0xff6600) //make it monero orange
+				// .setDescription('Informative text to add at start')
+				.addField('Hash', blockDetails.hash, false)
+				.addField('Diff', blockDetails.diff, true)
+				.addField('Height', blockDetails.height, true)
+				.addField('Value', blockDetails.value, true)
+				.addField('Effort', (effort * 100).toFixed(2) + '%', true);
 		
 			channel.send(embed);
 
